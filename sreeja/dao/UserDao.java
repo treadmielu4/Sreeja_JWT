900dfc1a1f5cbea83a9dee5e50118093ead59225
Treadmielu4 <72971776+treadmielu4@users.noreply.github.com>
2020-11-18 10:37:26 +0530

    Add files via upload

---
+++ sreeja/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.sreeja.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.simran.model.DAOUser;
+
+@Repository
+public interface UserDao extends CrudRepository<DAOUser, Integer> {
+	
+	DAOUser findByUsername(String username);
+	
+}
